4cabe3f838e932e1f7ca79fb647621fa9b82d330
Nour Aoude <naoude@gmail.com>
2023-08-24 15:32:25 +0300

    graded assignment 1 v7

--- week-3/graded-assignment-1.sql
+++ week-3/graded-assignment-1.sql
@@ -11,7 +11,8 @@ WITH RENTALS_BY_RATING AS
 		ON myinventory.inventory_id = myrental.inventory_id
 	INNER JOIN public.film as myfilm
 		ON myinventory.film_id = myfilm.film_id
-	WHERE EXTRACT(YEAR FROM myrental.rental_date) = 2005
+	WHERE 
+		EXTRACT(YEAR FROM myrental.rental_date) = 2005
 	GROUP BY 
 		myfilm.rating
 )
@@ -39,28 +40,11 @@ OR
 	LOWER(CAST(myfilm.rating AS TEXT)) = 'g'
 GROUP BY 
 	mycategory.name
-HAVING AVG(myfilm.rental_duration) > 5
+HAVING 
+	AVG(myfilm.rental_duration) > 5
 
 -- Determine the total rental amount collected from each customer. 
 -- List only those customers who have spent more than $100 in total.
-
-WITH RENTAL_AMOUNT_BY_CUSTOMER AS (
-SELECT  
-	mypayment.customer_id as customer_id,
-	SUM(mypayment.amount) as total_rental_amount
-FROM public.rental as myrental
-INNER JOIN public.payment as mypayment
-	ON myrental.rental_id = mypayment.rental_id
-GROUP BY 
-	mypayment.customer_id
-)
-SELECT
-	*
-FROM RENTAL_AMOUNT_BY_CUSTOMER
-WHERE 
-	total_rental_amount > 100
-
---OR
 
 WITH RENTAL_AMOUNT_BY_CUSTOMER AS (
 SELECT  
@@ -170,78 +154,87 @@ WHERE RENTED_FILMS.film_id IS NULL
 -- (INNER JOIN): Find the names of customers who rented films with a replacement cost greater 
 --than $20 and which belong to the 'Action' or 'Comedy' categories.
 
-WITH SELECTED_CUSTOMERS AS 
-(
 SELECT 
-DISTINCT mycustomer.customer_id
-FROM rental as myrental
-INNER JOIN inventory as myinventory
-ON myrental.inventory_id = myinventory.inventory_id
-INNER JOIN film as myfilm
-ON myinventory.film_id = myfilm.film_id
-INNER JOIN customer as mycustomer 
-ON myrental.customer_id = mycustomer.customer_id
-INNER JOIN film_category as myfilm_category
-ON myfilm_category.film_id = myfilm.film_id
-INNER JOIN category as mycategory
-ON myfilm_category.category_id = mycategory.category_id
-WHERE myfilm.replacement_cost > 20
-AND LOWER(mycategory.name) = 'action' OR LOWER(mycategory.name) = 'comedy'
-)
-SELECT
-CONCAT(mycustomer2.first_name, ' ', mycustomer2.last_name) as customer_name
-FROM SELECTED_CUSTOMERS
-INNER JOIN customer as mycustomer2
-ON SELECTED_CUSTOMERS.customer_id = mycustomer2.customer_id
+	DISTINCT mycustomer.customer_id,
+	CONCAT(mycustomer.first_name, ' ', mycustomer.last_name) as customer_name
+	FROM public.rental as myrental
+INNER JOIN public.inventory as myinventory
+	ON myrental.inventory_id = myinventory.inventory_id
+INNER JOIN public.film as myfilm
+	ON myinventory.film_id = myfilm.film_id
+INNER JOIN public.customer as mycustomer 
+	ON myrental.customer_id = mycustomer.customer_id
+INNER JOIN public.film_category as myfilm_category
+	ON myfilm_category.film_id = myfilm.film_id
+INNER JOIN public.category as mycategory
+	ON myfilm_category.category_id = mycategory.category_id
+WHERE 
+	myfilm.replacement_cost > 20
+AND 
+	LOWER(mycategory.name) = 'action' 
+OR 
+	LOWER(mycategory.name) = 'comedy'
 
 -- (LEFT JOIN): List all actors who haven't appeared in a film with a rating of 'R'.
 WITH ACTORS_IN_R_FILMS AS
 (
 SELECT 
-DISTINCT myactor.actor_id,
-myfilm.rating
-FROM actor as myactor
-LEFT OUTER JOIN film_actor as myfilm_actor
-ON myfilm_actor.actor_id = myactor.actor_id
+	DISTINCT myactor.actor_id,
+	myfilm.rating
+FROM public.actor as myactor
+LEFT OUTER JOIN public.film_actor as myfilm_actor
+	ON myfilm_actor.actor_id = myactor.actor_id
 LEFT OUTER JOIN film as myfilm
-ON myfilm_actor.film_id = myfilm.film_id
-WHERE LOWER(CAST(myfilm.rating AS TEXT)) = 'r'
+	ON myfilm_actor.film_id = myfilm.film_id
+WHERE 
+	LOWER(CAST(myfilm.rating AS TEXT)) = 'r'
 )
 SELECT
-myactor2.actor_id,
-CONCAT(myactor2.first_name, ' ', myactor2.last_name) as actor_name
-FROM actor as myactor2
+	myactor2.actor_id,
+	CONCAT(myactor2.first_name, ' ', myactor2.last_name) as actor_name
+FROM public.actor as myactor2
 LEFT OUTER JOIN ACTORS_IN_R_FILMS
-ON myactor2.actor_id = ACTORS_IN_R_FILMS.actor_id
+	ON myactor2.actor_id = ACTORS_IN_R_FILMS.actor_id
 WHERE ACTORS_IN_R_FILMS.actor_id IS NULL
+
+-- OR using a subquery:
+
+SELECT
+	myactor2.actor_id,
+	CONCAT(myactor2.first_name, ' ', myactor2.last_name) as actor_name
+FROM public.actor as myactor2
+WHERE myactor2.actor_id NOT IN (
+SELECT 
+	actor_id
+	FROM ACTORS_IN_R_FILMS
+)
 
 -- (Combination of INNER JOIN and LEFT JOIN): 
 -- Identify customers who have never rented a film from the 'Horror' category.
 
-WITH HORROR_RENTALS AS 
+WITH CUSTOMERS_WHO_RENTED_HORROR AS 
 (
 	SELECT
-	myrental.rental_id as rental_id,
-	myrental.customer_id as customer_id,
-	mycategory.name as category_name
-	FROM rental as myrental
-	INNER JOIN inventory as myinventory
-	ON myrental.inventory_id = myinventory.inventory_id
-	INNER JOIN film as myfilm
-	ON myinventory.film_id = myfilm.film_id
-	INNER JOIN film_category as myfilm_category
-	ON myfilm_category.film_id = myfilm.film_id
-	INNER JOIN category as mycategory
-	ON mycategory.category_id = myfilm_category.category_id
-	WHERE LOWER(mycategory.name) = 'horror'
+		DISTINCT myrental.customer_id as customer_id
+	FROM public.rental as myrental
+	INNER JOIN public.inventory as myinventory
+		ON myrental.inventory_id = myinventory.inventory_id
+	INNER JOIN public.film as myfilm
+		ON myinventory.film_id = myfilm.film_id
+	INNER JOIN public.film_category as myfilm_category
+		ON myfilm_category.film_id = myfilm.film_id
+	INNER JOIN public.category as mycategory
+		ON mycategory.category_id = myfilm_category.category_id
+	WHERE 
+		LOWER(mycategory.name) = 'horror'
 )
 SELECT 
-mycustomer.customer_id,
-CONCAT(mycustomer.first_name, ' ', mycustomer.last_name)
-FROM customer as mycustomer
-LEFT OUTER JOIN HORROR_RENTALS
-ON HORROR_RENTALS.customer_id = mycustomer.customer_id
-WHERE HORROR_RENTALS.rental_id IS NULL
+	CONCAT(mycustomer.first_name, ' ', mycustomer.last_name) as customer_name
+FROM public.customer as mycustomer
+LEFT OUTER JOIN CUSTOMERS_WHO_RENTED_HORROR
+	ON CUSTOMERS_WHO_RENTED_HORROR.customer_id = mycustomer.customer_id
+WHERE 
+	CUSTOMERS_WHO_RENTED_HORROR.customer_id IS NULL
 
 -- (Multiple INNER JOINs): Find the names and email addresses of customers who rented films directed 
 --  by a specific actor (let's say, for the sake of this task, that the actor's first name is 
@@ -249,19 +242,21 @@ WHERE HORROR_RENTALS.rental_id IS NULL
 --  although this might not match actual data in the DVD Rental database).
 
 SELECT 
-DISTINCT(mycustomer.customer_id),
-CONCAT(mycustomer.first_name, ' ', mycustomer.last_name) as customer_name,
-mycustomer.email 
-FROM actor as myactor
-INNER JOIN film_actor as myfilm_actor
-ON myactor.actor_id = myfilm_actor.actor_id
-INNER JOIN film as myfilm
-ON myfilm_actor.film_id = myfilm.film_id
-INNER JOIN inventory as myinventory
-ON myfilm.film_id = myinventory.film_id
-INNER JOIN rental as myrental
-ON myinventory.inventory_id = myrental.inventory_id
-INNER JOIN customer as mycustomer
-ON myrental.customer_id = mycustomer.customer_id
-WHERE myactor.first_name = 'Nick'
-AND myactor.last_name = 'Wahlberg'
+	DISTINCT(mycustomer.customer_id),
+	CONCAT(mycustomer.first_name, ' ', mycustomer.last_name) as customer_name,
+	mycustomer.email 
+FROM public.actor as myactor
+INNER JOIN public.film_actor as myfilm_actor
+	ON myactor.actor_id = myfilm_actor.actor_id
+INNER JOIN public.film as myfilm
+	ON myfilm_actor.film_id = myfilm.film_id
+INNER JOIN public.inventory as myinventory
+	ON myfilm.film_id = myinventory.film_id
+INNER JOIN public.rental as myrental
+	ON myinventory.inventory_id = myrental.inventory_id
+INNER JOIN public.customer as mycustomer
+	ON myrental.customer_id = mycustomer.customer_id
+WHERE 
+	myactor.first_name = 'Nick'
+AND 
+	myactor.last_name = 'Wahlberg'
